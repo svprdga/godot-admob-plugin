a967b5107495661640c8026875c045b8db7bfc90
svprdga <svprdga@reprom.io>
2021-07-06 19:14:01 +0200

    temp

--- android-plugin/build.sh
+++ android-plugin/build.sh
@@ -1,4 +1,4 @@
 # Builds the plugin and moves it to the appropriate plugins folder in the tester project.
 ./gradlew assembleDebug &&
-	cp godotadmobandroidplugin/build/outputs/aar/godotadmobandroidplugin-release.aar ../tester/android/plugins/godot-admob-plugin/godot-admob-android-plugin.aar
+	cp godotadmobandroidplugin/build/outputs/aar/godotadmobandroidplugin-release.aar ../tester/android/plugins/godot-admob-android-plugin.aar
 
