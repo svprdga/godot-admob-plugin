eff340c74324ec836b8ea820f1282eb90d37d099
David <svprdga@reprom.io>
2021-07-07 10:37:27 +0200

    GodotTester can load the plugin and output the hello phrase

--- godot-tester/World.gd
+++ godot-tester/World.gd
@@ -4,6 +4,7 @@ onready var status_label = $StatusLabel
 
 func _ready():
 	if Engine.has_singleton("GodotAdmobIosPlugin"):
-		status_label.text = "Plugin successfully loaded."
+		var singleton = Engine.get_singleton("GodotAdmobIosPlugin")	
+		status_label.text = "Plugin successfully loaded: " + singleton.say_hello()
 	else:
 		status_label.text = "*** Plugin could not be loaded. ***"
