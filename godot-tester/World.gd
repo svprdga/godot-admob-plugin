a7e8162c2f855d47b6eb9a72830bee8df27bb769
svprdga <svprdga@reprom.io>
2021-07-07 11:01:00 +0200

    GodotTester can load android plugin

--- godot-tester/World.gd
+++ godot-tester/World.gd
@@ -3,8 +3,13 @@ extends Node2D
 onready var status_label = $StatusLabel
 
 func _ready():
-	if Engine.has_singleton("GodotAdmobIosPlugin"):
-		var singleton = Engine.get_singleton("GodotAdmobIosPlugin")	
-		status_label.text = "Plugin successfully loaded: " + singleton.say_hello()
+	var singleton = null
+	if OS.get_name() == "Android" and Engine.has_singleton("GodotAdmobAndroidPlugin"):
+			singleton = Engine.get_singleton("GodotAdmobAndroidPlugin")
+	elif OS.get_name() =="iOS" and Engine.has_singleton("GodotAdmobIosPlugin"):
+			singleton = Engine.get_singleton("GodotAdmobIosPlugin")	
+	
+	if singleton != null:
+		status_label.text = "Plugin successfully loaded: %s" % singleton.say_hello()
 	else:
 		status_label.text = "*** Plugin could not be loaded. ***"
