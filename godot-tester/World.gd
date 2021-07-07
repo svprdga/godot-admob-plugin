b6b4a76015a55a8c6df3101d8d6d0b30bc4560d6
David <svprdga@reprom.io>
2021-07-07 10:30:08 +0200

    GodotTester can load the plugin

--- godot-tester/World.gd
+++ godot-tester/World.gd
@@ -1,16 +1,9 @@
 extends Node2D
 
+onready var status_label = $StatusLabel
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if Engine.has_singleton("GodotAdmobIosPlugin"):
+		status_label.text = "Plugin successfully loaded."
+	else:
+		status_label.text = "*** Plugin could not be loaded. ***"
